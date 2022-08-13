26b242a5579620fc1ca6e4972f57941c344485ea
Under <101993538+Under-117@users.noreply.github.com>
2022-08-13 15:20:35 -0300

    Updated page link to Educação Nutricional

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function redirect(number) {
   if(number == 1) {
     location.replace("https://under-117.github.io/Jornal-Escola-EducacaoSexual/")
   } else if (number == 2) {
-    location.replace("https://github.com/Under-117")
+    location.replace("https://under-117.github.io/Jornal-Escola-Educacao-Nutricional/")
   }
 }
 function changeThemes() {
